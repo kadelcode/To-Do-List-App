83bf7b97e3fef2b7ceb83ddd4403eac2dff1a1f9
Ayodele Kadiri <kadelcode@gmail.com>
2024-07-03 13:49:44 +0100

    initial setup

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const taskForm = document.getElementById('task-form');
+	const taskInput = document.getElementById('task-input');
+	const taskList = document.getElementById('task-list');
+
+	loadTasks();
+});
