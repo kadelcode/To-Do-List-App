7fd539394c31e16c2f63ff9e63e0b8c9dc23001f
Ayodele Kadiri <kadelcode@gmail.com>
2024-07-06 22:28:06 +0100

    Handling form submission

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 	const taskList = document.getElementById('task-list');
 
 	loadTasks();
+
+	taskForm.addEventListener('submit', (e) => {
+		e.preventDefault(); // prevents the form from reloading the page
+		add(taskInput.value);
+
+		// set the task input value to empty after adding the task
+		taskInput.value = '';
+	});
 });
