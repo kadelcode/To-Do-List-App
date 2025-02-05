94c2ae7d1265241f9f55d101ecfea3a0082aeb2c
kadelcode <kadelcode@gmail.com>
2025-02-05 10:45:28 +0100

    Include necessary headers

---
+++ C/todo.c
@@ -0,0 +1,8 @@
+// Provides input/output functions like printf and scanf
+#include <stdio.h>
+
+// Provides functions like exit for program termination
+#include <stdlib.h>
+
+// Provides string manipulation functions like strcspn
+#include <string.h>
